e3543e9085e6d013bcbfcbfc5eb89fd06a7cbc9f
Lucas Lemos <lucaslb88@gmail.com>
2016-09-18 16:11:52 -0300

    Criação de layout inicial da tela de login

---
+++ CamaraAlerta/app/src/main/java/br/com/petasoft/camaraalerta/LoginActivity.java
@@ -0,0 +1,27 @@
+package br.com.petasoft.camaraalerta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Lucas on 9/18/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+
+    public void normalLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
