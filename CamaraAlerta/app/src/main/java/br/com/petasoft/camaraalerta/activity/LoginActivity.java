a8a27c0533fe828dba98ab9a64c36432824c708b
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2016-11-02 23:08:38 -0200

    Login pelo facebook com WS

--- CamaraAlerta/app/src/main/java/br/com/petasoft/camaraalerta/activity/LoginActivity.java
+++ CamaraAlerta/app/src/main/java/br/com/petasoft/camaraalerta/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package br.com.petasoft.camaraalerta.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.TextViewCompat;
@@ -21,6 +22,10 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -37,6 +42,8 @@ import com.google.gson.JsonParser;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -48,11 +55,14 @@ import model.Configuration;
  * Created by Lucas on 9/18/2016.
  */
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
     private static Configuration configuration;
     private LoginButton loginButton;
     private EditText editLogin, editPass;
     private CallbackManager callbackManager;
+    private Profile profile;
+    private LoginResult loginResul;
+    private ProgressDialog progressDialog;
 
     private Intent intent;
 
@@ -74,15 +84,38 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         editLogin = (EditText) findViewById(R.id.editLogin);
         editPass = (EditText) findViewById(R.id.editPassword);
 
-        loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("public_profile", "email");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-                System.out.println("onSuccess");
+                loginResul = loginResult;
+                final Profile profile = Profile.getCurrentProfile();
+                String accessToken = loginResult.getAccessToken().getToken();
+                Log.i("accessToken", accessToken);
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.i("LoginActivity", response.toString());
+                        // Get facebook data from login
+                        Bundle bFacebookData = getFacebookData(object);
+                        try {
+                            Log.i("Email", object.getString("email"));
+                            Log.i("Name", profile.getName());
+                            facebookLogin(loginResul,object.getString("email"),profile.getName(),object.getString("id"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location"); // Parámetros que pedimos a facebook
+                request.setParameters(parameters);
+                request.executeAsync();
             }
+            //facebookLogin(loginResult);
+
 
             @Override
             public void onCancel() {
@@ -119,8 +152,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
@@ -157,7 +189,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e(TAG,"onConnectionFailed "+connectionResult.getErrorMessage());
+        Log.e(TAG, "onConnectionFailed " + connectionResult.getErrorMessage());
     }
 
     // Função para realizar login com e-mail/login e senha
@@ -165,10 +197,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         intent = new Intent(this, MainActivity.class);
         //Realiza conexão com o webservice para realizar o login
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = configuration.base_url+"user/login";
+        String url = configuration.base_url + "user/login";
         StringRequest getRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
+                new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // response
@@ -183,11 +214,11 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                             /* Transforma o JSON em um objeto Cidadao
                              * Grava o cidadao no objeto estático de configurações para ser acessado
                              * por qualquer arquivo.*/
-                            configuration.usuario = configuration.gson.fromJson(mJson,Cidadao.class);
-                            if(configuration.usuario == null){
+                            configuration.usuario = configuration.gson.fromJson(mJson, Cidadao.class);
+                            if (configuration.usuario == null) {
                                 Log.i("Error", "não foi possível realizar o login");
-                            }else{
-                                Toast toast = Toast.makeText(getApplicationContext(), "Login efetuado com sucesso", Toast.LENGTH_LONG );
+                            } else {
+                                Toast toast = Toast.makeText(getApplicationContext(), "Login efetuado com sucesso", Toast.LENGTH_LONG);
                                 toast.show();
                                 //Redireciona a aplicação para a tela principal
                                 startActivity(intent);
@@ -200,12 +231,11 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
                     }
                 },
-                new Response.ErrorListener()
-                {
+                new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // error
-                        if(error.networkResponse != null && error.networkResponse.data != null) {
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
                             String result = new String(error.networkResponse.data);
                             try {
 
@@ -216,7 +246,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        }else{
+                        } else {
                             Toast toast = Toast.makeText(getApplicationContext(), "Não foi possível se conectar com o servidor", Toast.LENGTH_LONG);
                             toast.show();
                         }
@@ -224,9 +254,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 }
         ) {
             @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String>  params = new HashMap<String, String>();
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
                 params.put("login", editLogin.getText().toString());
                 params.put("senha", editPass.getText().toString());
 
@@ -238,15 +267,122 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         finish();
     }
 
-    public void registrarConta(View view){
+    public void facebookLogin(final LoginResult loginResult, final String email,final String name, final String token) {
+        //Realiza conexão com o webservice para realizar o login
+        Log.i("Facebook Login", "Chamada da funcao");
+        intent = new Intent(LoginActivity.this, MainActivity.class);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = configuration.base_url + "user/facebookLogin";
+        StringRequest getRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //Realiza o parser do JSON vindo do WebService
+                            JSONObject json = new JSONObject(response);
+                            JsonParser parser = new JsonParser();
+                            JsonElement mJson = parser.parse(json.getString("data"));
+                            /* Transforma o JSON em um objeto Cidadao
+                             * Grava o cidadao no objeto estático de configurações para ser acessado
+                             * por qualquer arquivo.*/
+                            configuration.usuario = configuration.gson.fromJson(mJson, Cidadao.class);
+                            Log.i("Facebook Login", "Envio do rest");
+                            if (configuration.usuario == null) {
+                                Log.i("Error", "não foi possível realizar o login");
+                            } else {
+                                Log.i("Name", configuration.usuario.getNome());
+                                Toast toast = Toast.makeText(getApplicationContext(), "Login efetuado com sucesso", Toast.LENGTH_LONG);
+                                toast.show();
+                                //Redireciona a aplicação para a tela principal
+                                startActivity(intent);
+                                finish();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("Error", "Error response");
+                        // error
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            String result = new String(error.networkResponse.data);
+                            try {
+
+                                JSONObject json = new JSONObject(result);
+
+                                Toast toast = Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_LONG);
+                                toast.show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Não foi possível se conectar com o servidor", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("token", token);
+                params.put("email",email);
+                params.put("nome", name);
+
+                return params;
+            }
+        };
+        queue.add(getRequest);
+
+    }
+
+    public void registrarConta(View view) {
         Intent intent = new Intent(this, Registrar.class);
         startActivity(intent);
     }
 
-    public void esqueciSenha(View view){
+    public void esqueciSenha(View view) {
         Intent intent = new Intent(this, EsqueciSenha.class);
         startActivity(intent);
     }
 
 
+    private Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+
+            URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+            bundle.putString("profile_pic", profile_pic.toString());
+
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
