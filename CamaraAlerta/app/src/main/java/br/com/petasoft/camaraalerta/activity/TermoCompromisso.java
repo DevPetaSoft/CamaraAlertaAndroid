5a87c7c2a5c5cd1786205999fdac1d830612e088
Lucas Lemos <lucaslb88@gmail.com>
2017-02-14 17:21:51 -0200

    Adição do botão de voltar na tela de Termos e Condições

--- CamaraAlerta/app/src/main/java/br/com/petasoft/camaraalerta/activity/TermoCompromisso.java
+++ CamaraAlerta/app/src/main/java/br/com/petasoft/camaraalerta/activity/TermoCompromisso.java
@@ -2,6 +2,9 @@ package br.com.petasoft.camaraalerta.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 import br.com.petasoft.camaraalerta.R;
 
 public class TermoCompromisso extends AppCompatActivity {
@@ -10,5 +13,20 @@ public class TermoCompromisso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_termo_compromisso);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarTermo);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
